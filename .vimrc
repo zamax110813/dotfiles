006c7ddf45dfeb803197759bf2c977620db5ed71
zamax110813 <zamax110813@gmail.com>
2022-05-02 16:57:45 +0900

    anything

--- .vimrc
+++ .vimrc
@@ -87,3 +87,7 @@ set laststatus=2
 set wildmode=list:longest
 
 inoremap <silent> jj <ESC>
+
+"tabstop"
+set tabstop=4
+set shiftwidth=4
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/colors ~/.vim
+ln -sf ~/dotfiles/ftdetect ~/.vim
