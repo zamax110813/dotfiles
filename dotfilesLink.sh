7aabacb0a3fb3067df523a1f28c5d6ca5f67c4ae
zamax110813 <zamax110813@gmail.com>
2022-05-02 17:54:03 +0900

    add dein

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,5 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/colors ~/.vim
 ln -sf ~/dotfiles/ftdetect ~/.vim
+ln -sf ~/dotfiles/dein/dein.toml ~/.vim/dein.toml
+ln -sf ~/dotfiles ~/.vim
