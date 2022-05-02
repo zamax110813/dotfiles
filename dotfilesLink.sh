c00c1fd5735ac371405f7b3e41b9c39eb0ce00c9
zamax110813 <zamax110813@gmail.com>
2022-05-02 18:59:31 +0900

    j

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/colors ~/.vim
-ln -sf ~/dotfiles/ftdetect ~/.vim
-ln -sf ~/dotfiles/dein/dein.toml ~/.vim/dein.toml
-ln -sf ~/dotfiles ~/.vim
+ln -sf ~/dotfiles/.vim ~/.vim
